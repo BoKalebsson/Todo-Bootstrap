48ee593c2b6e3e4cc4f1460089f09858238858ca
BoKalebsson <bo_kalebsson@hotmail.com>
2025-10-30 16:39:36 +0100

    Implemented eventlistener for todo-section, implemented remove-card function.

--- script.js
+++ script.js
@@ -20,16 +20,20 @@ function getFormData(){
 }
 
 // Event listener for the Add Todo button
-// Purpose: when user clicks, gather form data, add createdAt-timestamp, and log it.
+// Purpose: when user clicks, gather form data, add createdAt-timestamp,
+// add id-timestamp, and then create card with createToDoItem(data),
+// and reset the form.
 addButton.addEventListener("click", function() {
     const data = getFormData();
     data.createdAt = new Date();
-    createToDoItem(data);    
+    data.id = Date.now();
+    createToDoItem(data);
+    document.getElementById("todoForm").reset();    
 });
 
 function createToDoItem(data){
     const cardHTML = `
-    <div class="card m-3 p-2">
+    <div class="card m-3 p-2" data-id="${data.id}">
         <div class="d-flex justify-content-between align-items-start">
             <div>
             <h6 class="mb-1"><b>${data.title}</b></h6>
@@ -39,13 +43,13 @@ function createToDoItem(data){
     <div class="d-flex align-items-start gap-2">
         <small class="text-muted">Created: ${data.createdAt.toLocaleString()}</small>
     <div class="btn-group">
-      <button class="btn btn-outline-success btn-sm">
+      <button class="btn btn-outline-success btn-sm complete-todo">
         <i class="bi bi-check-lg"></i>
       </button>
-      <button class="btn btn-outline-primary btn-sm">
+      <button class="btn btn-outline-primary btn-sm edit-todo">
         <i class="bi bi-pencil"></i>
       </button>
-      <button class="btn btn-outline-danger btn-sm">
+      <button class="btn btn-outline-danger btn-sm delete-todo">
         <i class="bi bi-trash"></i>
       </button>
     </div> 
@@ -62,3 +66,19 @@ function createToDoItem(data){
 `
 todoList.innerHTML += cardHTML;
 }
+
+// Event listener for the cards todo-list
+// Purpose: when user clicks a button, the eventlistener will find out what button,
+// then trigger the correct response, to the closest card.
+todoList.addEventListener("click", function(event){
+
+    const target = event.target;
+
+    // Delete card-method():
+    if (target.closest(".delete-todo")){
+        const card = target.closest(".card");
+        card.remove();
+        return;
+    }
+
+});
